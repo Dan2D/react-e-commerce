da811d0705d5584cc9669f5e3bf169e8f3db1d87
Dan Schultz <travelguy90@gmail.com>
2019-10-22 15:51:52 -0700

    (create): Made components for homepage and stlyed menu-item component

---
+++ src/components/directory/Directory.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {sections} from "./directory.data";
+
+import MenuItem from "../menu-item/MenuItem";
+import "./directory.styles.scss";
+
+export default class Directory extends Component {
+    constructor(){
+        super();
+        this.state = {
+            sections: sections,
+        }
+    }
+    render() {
+        return (
+            <div className='directory-menu'>
+                {
+                    this.state.sections.map(({title, imageUrl, id, size}) => ( 
+                            <MenuItem 
+                                key={id} 
+                                title={title}
+                                imageUrl={imageUrl}
+                                size={size} />
+                        )
+                    )
+                }
+            </div>
+        )
+    }
+}
