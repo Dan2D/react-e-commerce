99b80d153e520c77f7a828a5b4610b2a3d8b7a54
Dan Schultz <travelguy90@gmail.com>
2019-10-23 08:13:26 -0700

    (create): Added header component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
+import Header from "./components/header/Header";
 import HomePage from "./pages/homepage/HomePage";
 import ShopPage from "./pages/shop page/ShopPage";
-import "./App.css";
+import "./App.scss";
 
 
 function App() {
   return (
     <div>
-      <Router>
+        <Header />
         <Switch>
             <Route exact path="/" component={HomePage} />
             <Route  path="/shop" component={ShopPage} />
         </Switch>
-      </Router>
     </div>
   );
 }
